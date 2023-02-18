73b62bb855ce91a74f94593715c5f12198d7a01a
YoloOfficial <113841593+YoloOfficial@users.noreply.github.com>
2023-02-18 17:37:53 +0500

    initial cv website files

---
+++ first.html.php
@@ -0,0 +1,30 @@
+<html>
+  <head></head>
+  <body>
+    <h1>Today</h1>
+    <p>Today is saturday</p>
+   <ul>
+    <li>Milk</li>
+    <li>Cheese
+        <ul>
+            <li>Blue cheese</li>
+            <li>Feta</li>
+        </ul>
+    </li>
+</ul>
+<img scr="Anglena/image.jpg" alt="yu"></img>
+  <a href="next.php">google</a>
+<table>
+  <thead>
+    <tr>
+      <th>Date</th>
+        <th>Time</th>
+   
+    </tr>
+   
+  </thead>
+  <tbody></tbody>
+  <tfoot></tfoot>
+</table>
+  </body>
+</html>
